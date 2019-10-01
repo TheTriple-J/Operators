bb4ef00ae2e714c31bbcbe26a26e0f51e753651e
Joe <joe_jackson1@hotmail.com>
2019-10-01 17:09:56 +0100

    Operators, switch
    Lecture 2 CT4029

---
+++ More or Less than 5/More or Less than 5/main.cpp
@@ -0,0 +1,104 @@
+#include <iostream> \\Enables output and input stream
+
+//int main()
+//{
+//
+//	std::cout << "Enter any number between 0-10: ";
+//
+//	int enteredValue;
+//	std::cin >> enteredValue;
+//
+//	if (enteredValue < 5)
+//	{
+//		std::cout << "Enter any number between 0-10: ";
+//	}
+//	else if (enteredValue > 5)
+//	{
+//		std::cout << "Enter any number between 0-10: ";
+//	}
+//	else if (enteredValue ==5)
+//	{
+//		std::cout << "Enter number equal to 5: ";
+//	}
+//	else
+//	{
+//		std::cout << "Enter an invalid value: ";
+//    }
+//}
+//int main()
+//{
+//
+//	std::cout << "Enter any number between 0-10: ";
+//
+//	int enteredValue;
+//	std::cin >> enteredValue;
+//
+//	if (enteredValue == 0)
+//	{
+//		std::cout << "U Grade: ";
+//	}
+//	else if (enteredValue >= 1 && enteredValue <= 39)
+//	{
+//		std::cout << "F Grade: ";
+//	}
+//	else if (enteredValue >= 40 && enteredValue <= 49)
+//	{
+//		std::cout << "E Grade: ";
+//	}
+//	else if (enteredValue >= 50 && enteredValue <= 59)
+//	{
+//		std::cout << "D Grade: ";
+//    }
+//	else if (enteredValue >= 60 && enteredValue <= 69)
+//	{
+//		std::cout << "C Grade: ";
+//	}
+//	else if (enteredValue >= 70 && enteredValue <= 79)
+//	{
+//		std::cout << "B Grade: ";
+//	}
+//	else (enteredValue >= 80)
+//	{
+//		std::cout << "A Grade: ";
+//	}
+//}
+//int main(
+//	{
+//		std::cout << "Enter any number: ";
+//int enteredValue
+//std::cin >> enteredValue; 
+//
+//
+//if (enteredValue )
+//{
+//	std::cout << "The entered number is even: ";
+//}
+//else if (enteredValue )
+//{
+//	std::cout << "The entered number is odd: ";
+//}
+//else if (enteredValue )
+//{
+//	std::cout << "The entered number is 0: ";
+//}
+//else(enteredValue )
+//{
+//	std::cout << "The entered number is invalid: ";
+//}
+//	}
+int main()
+{
+	std::cout << "You are in a field with zombies, what is your next action?" << std::endl;
+
+	std::cout << "You choose to start dancing with the zombies: " std::endl;
+	std::cout << "You pick up a log and start to kill the zombies: "std::endl;
+	std::cout << "you run towards a house to try and get shelter from the zombies: "std::endl;
+	std::cout << "You jump into a close by river to get away from the zombies: "std::endl;
+	 int 
+	
+		 
+		 switch 
+}
+
+
+
